2d2aff337dbea52657af3419a16af75ebd7774be
Rehan Makrani <rehanmakrani727@gmail.com>
2025-01-07 00:33:57 +0530

    Bug fixes for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
